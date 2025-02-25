892328f9ecfb33ac511e7a4c0ae158d630e147a0
Neetu Sarkar <neetusarkarmartin9@gmail.com>
2025-02-26 01:16:37 +0530

    files changed

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,18 @@
-// Protect specific routes
-export default {
-  publicRoutes: ["/", "/auth/signin", "/auth/signup"], // Routes that don't require authentication
-};
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Optional: Ensure Next.js recognizes this as middleware
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)"]);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+});
+
 export const config = {
-  matcher: ["/((?!_next).*)"], // Apply to all routes except static files
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
